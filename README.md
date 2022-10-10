62ac6f196643c56a49e6d91404bf7f3d886afa84
Arnaud Aubert <109612999+mlhnono68@users.noreply.github.com>
2022-10-10 10:52:51 +0200

    Mysql as feature (#2)
    * Adding a variable for mySQL
    * Removing warning message and additional support for password change
    * Better README
    * Reduce the number of Docker layers

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
-Docker image for future pterodactly egg to support Azorium
+Pterodactyl egg and docker image for future pterodactly egg to support Azorium
 
 # How to build ?
 ```shell
 echo $PAT | docker login ghcr.io --username mlhnono68 --password-stdin
 docker build -t azorium-egg .
+```
+
+# How to publish ?
+```shell
 docker tag azorium-egg ghcr.io/mlhnono68/azorium-egg:0.0.1
 docker push ghcr.io/mlhnono68/azorium-egg:0.0.1
-
-docker pull ghcr.io/mlhnono68/azorium-egg:0.0.1
-
-sudo rm -rf /Users/auberta2/Downloads/container/* && docker build -t azorium-egg . && docker run -p 80:80 -p3306:3306 --read-only -v /Users/auberta2/Downloads/container:/home/container -it azorium-egg
 ```
+
+# How to build and run locally ?
+```shell
+sudo rm -rf ./container && mkdir -p ./container && docker build -t azorium-egg . && docker run -p 2000:2000 --env SERVER_IP=0.0.0.0 --env SERVER_PORT=2000 --env EMBEDDED_MYSQL_PASSWORD=password --read-only -v $PWD/container:/home/container -it azorium-egg
+```
+
+# How to install and use the egg ?
+1) Simply import the egg file [egg-azorium.json](./egg-azorium.json) into one of your Pterodactyl nest
+2) Create your server and bind it to an allocation that will handle the web traffic. If you have a database host available, create a database for your Azorium server. Otherwise, define a value for `EMBEDDED_MYSQL_PASSWORD` and an embedded database will be created for you.
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,52 +1,63 @@
 #!/bin/sh
 
+# Create the directories where variable nginx and php data will be stored as root is readonly
 mkdir -p /home/container/lib-nginx
 mkdir -p /home/container/log-nginx
 mkdir -p /home/container/run-nginx
 mkdir -p /home/container/log-php
-
 mkdir -p /home/container/tmp
 TMPDIR=/home/container/tmp
 
-if [ ! -d /home/container/mysql ]
+# Only start mysql if EMBEDDED_MYSQL_PASSWORD is defined
+if [ ! -z "$EMBEDDED_MYSQL_PASSWORD" ]
 then
-    mkdir -p /home/container/mysql/data
-    mysql_install_db --tmpdir=/home/container/tmp --datadir=/home/container/mysql/data
+    # Only initialize the system database if not already done
+    if [ ! -d /home/container/mysql ]
+    then
+        echo "Initializing mySQL embedded database for Azorium..."
+        mkdir -p /home/container/mysql/data
+        mysql_install_db --tmpdir=/home/container/tmp --datadir=/home/container/mysql/data
+    fi
 
+    # Start mysqld in the background
+    echo "Starting mySQL embedded database for Azorium..."
+    /usr/bin/mysqld_safe --datadir='/home/container/mysql/data' --port=3306 --skip_networking=OFF --tmpdir=/home/container/tmp &
+    sleep 2
+
+    # Setup the Azorium database
+    echo "Preparing azuriom database and user for the mySQL embedded database..."
+    cat <<EOF | mysql -S /home/container/mysqld.sock
+    CREATE USER IF NOT EXISTS 'azuriom'@'127.0.0.1' IDENTIFIED BY '$EMBEDDED_MYSQL_PASSWORD';
+    CREATE DATABASE IF NOT EXISTS azuriom;
+    GRANT ALL PRIVILEGES ON azuriom.* TO 'azuriom'@'127.0.0.1' WITH GRANT OPTION;
+    ALTER USER 'azuriom'@'127.0.0.1' IDENTIFIED BY '$EMBEDDED_MYSQL_PASSWORD';
+EOF
 fi
-#echo "socket=/home/container/mysqld.sock" >> /etc/my.cnf
 
-/usr/bin/mysqld_safe --datadir='/home/container/mysql/data' --port=3306 --skip_networking=OFF --tmpdir=/home/container/tmp &
-
-sleep 2
-
-# Unzip Azuriom
+# Unzip Azuriom if not already done
 if [ ! -d /home/container/azuriom ]
 then
+    echo "Unzipping vanilla Azuriom..."
     mkdir -p /home/container/azuriom
     ( cd /home/container/azuriom && unzip /AzuriomInstaller.zip && chmod -R 755 /home/container/azuriom )
 fi
 
-# Setup the Azorium database
-cat <<EOF | mysql -S /home/container/mysqld.sock
-CREATE USER 'azuriom'@'127.0.0.1' IDENTIFIED BY 'password';
-CREATE DATABASE azuriom;
-GRANT ALL PRIVILEGES ON azuriom.* TO 'azuriom'@'127.0.0.1' WITH GRANT OPTION;
-EOF
-
 # Start PHP fpm engine
-set
+echo "Starting PHP..."
 php-fpm81 -F -O &
 
 # Start nginx in foreground
-# bash
+echo "Preparing nginx.conf..."
 cp /etc/nginx/nginx.conf /home/container/nginx.conf
 if [ ! -z "$SERVER_PORT" ]
 then
     echo Listening to $SERVER_IP:$SERVER_PORT
     sed -i'' "s/listen .*;/listen $SERVER_IP:$SERVER_PORT;/" /home/container/nginx.conf
-    cat /home/container/nginx.conf | grep listen
+else
+    >&2 echo "Error: SERVER_IP and SERVER_PORT should be defined by Pterodactyl allocation"
+    exit 1
 fi
 
-nginx -g 'daemon off;' -c /home/container/nginx.conf -e /home/container/log-nginx/nginx-error.log &
-bash
+# Start nginx in the foreground
+echo "Starting nginx..."
+nginx -g 'daemon off;' -c /home/container/nginx.conf -e /home/container/log-nginx/nginx-error.log
